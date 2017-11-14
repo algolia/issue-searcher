c3bb265b0cb3a3563468762bce3bc43fd438f707
Ronan Levesque <ronan.levesque@algolia.com>
2017-11-14 16:39:23 +0100

    Add missing viewbox

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,6 +13,7 @@ const Header = ({ authenticated, dispatch }) => (
         <Link className={styles.navLink} to="/">
           <svg
             className={styles.icon}
+            viewBox="0 0 24 24"
             width="24"
             height="24"
             xmlns="http://www.w3.org/2000/svg"
