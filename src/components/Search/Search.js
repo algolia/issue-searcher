57fba48f898583f427f03ba60fcacec8a4937364
Haroen Viaene <hello@haroen.me>
2017-11-14 15:42:18 +0100

    initial searchbar

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { SearchBox } from 'react-instantsearch/dom';
 import styles from './Search.module.css';
 
-const Search = () => <div className={styles.root} />;
+const Search = () => (
+  <div className={styles.root}>
+    <SearchBox />
+  </div>
+);
 
 export default Search;
