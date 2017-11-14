3cef69c5ffccdbff4f2eed524cc328e09550e6e7
Haroen Viaene <hello@haroen.me>
2017-11-14 12:56:20 +0100

    disable PR link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,37 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import styles from './Header.module.css'
+import React from 'react';
+import Link from 'gatsby-link';
+import styles from './Header.module.css';
 
 const Header = () => (
   <header className={styles.root}>
     <nav className={styles.nav}>
       <div className={styles.navLogo}>
-        <Link className={styles.navLink} to="/">issue-searcher</Link>
+        <Link className={styles.navLink} to="/">
+          issue-searcher
+        </Link>
       </div>
       <div>
-        <Link className={styles.navLink} to="/issues/">Issues</Link>
-        <Link className={styles.navLink} to="/pull-requests/">Pull requests</Link>
+        <Link className={styles.navLink} to="/issues/">
+          Issues
+        </Link>
+        <Link
+          className={styles.navLink}
+          to="/pull-requests/"
+          onClick={e => e.preventDefault()}
+          style={{ opacity: 0.5 }}
+        >
+          Pull requests
+        </Link>
         <span className={styles.navSeparator}>|</span>
-        <Link className={styles.navLink} to="/login/">Login</Link>
-        <Link className={styles.navLink} to="/settings/">Settings</Link>
+        <Link className={styles.navLink} to="/login/">
+          Login
+        </Link>
+        <Link className={styles.navLink} to="/settings/">
+          Settings
+        </Link>
       </div>
     </nav>
   </header>
-)
+);
 
-export default Header
+export default Header;
