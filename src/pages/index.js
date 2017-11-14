69fc705b4a115a188ca78d601f466eb530293ad7
Ronan Levesque <ronan.levesque@algolia.com>
2017-11-14 16:19:25 +0100

    Add basic stylings to text blocks

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,33 +5,37 @@ import Search from '../components/Search/Search';
 import Sidebar from '../components/Sidebar/Sidebar';
 import Results from '../components/Results/Results';
 
-const dotenv = `
-FALLBACK_UID=
-FALLBACK_API_KEY=
+const dotenv = `FALLBACK_UID=<your_uid>
+FALLBACK_API_KEY=<your_api_key>
 `;
 
 const privateLink = `github.algolia.com/private?${new Date().getTime()}`;
 const signinLink = 'github.algolia.com/signin';
 
 const IndexPage = () => (
-  <Grid>
-    <p>Welcome to the Issue Searcher.</p>
+  <div className="textBlock">
+    <h1>Welcome to the Issue Searcher.</h1>
     <p>This project was created during the Q4 Algolia InstantSearch offsite</p>
-    <p>
-      clone{' '}
-      <a href="https://github.com/algolia/issue-searcher">
-        algolia/issue-searcher
-      </a>
-    </p>
-    <p>
-      download the dependencies with <code>yarn</code>
-    </p>
-    <p>
-      Log in to{' '}
-      <a href={`https://${signinLink}`} target="_blank">
-        {signinLink}
-      </a>
-    </p>
+    <ol>
+      <li>
+        clone{' '}
+        <a href="https://github.com/algolia/issue-searcher">
+          algolia/issue-searcher
+        </a>
+      </li>
+      <li>
+        download the dependencies with <code>yarn</code>
+      </li>
+      <li>
+        Log in to{' '}
+        <a href={`https://${signinLink}`} target="_blank">
+          {signinLink}
+        </a>
+      </li>
+    </ol>
+    <p />
+    <p />
+    <p />
     <p>
       Then you can go to{' '}
       <a href={`https://${privateLink}`} target="_blank">
@@ -39,17 +43,18 @@ const IndexPage = () => (
       </a>
     </p>
     <p>
-      and enter that info in <code>.env.development</code>
+      And enter that info in <code>.env.development</code>:
     </p>
     <pre>{dotenv}</pre>
     <p>
-      run the site with <code>yarn develop</code>
+      Run the site with <code>yarn develop</code>.
     </p>
+    <hr />
     <small>
       Be careful, don't make this public, it'll allow everyone to search issues
       on all private repositories you have access to.
     </small>
-  </Grid>
+  </div>
 );
 
 export default IndexPage;
