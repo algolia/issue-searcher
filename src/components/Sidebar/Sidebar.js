b4cc887b97f49af5b8d4867f3f7c9164ccde376b
Haroen Viaene <hello@haroen.me>
2017-11-14 15:59:29 +0100

    provisionary refinements

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { RefinementList } from 'react-instantsearch/dom';
 import styles from './Sidebar.module.css';
 
-const Sidebar = () => <div className={styles.root} />;
+const Sidebar = () => (
+  <div className={styles.root}>
+    <RefinementList attributeName="repository.full_name" />
+    <RefinementList attributeName="login" />
+  </div>
+);
 
 export default Sidebar;
