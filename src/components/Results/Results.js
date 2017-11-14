5fece1a6fdaab8071447bf8ea68b5e7801feed5d
Haroen Viaene <hello@haroen.me>
2017-11-14 15:59:22 +0100

    provisionary results

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { Hits } from 'react-instantsearch/dom';
 import styles from './Results.module.css';
 
-const Results = () => <div className={styles.root} />;
+const Hit = ({ hit }) => (
+  <div>
+    <pre>{JSON.stringify(hit, null, '  ')}</pre>
+    <hr />
+  </div>
+);
+
+const Results = () => (
+  <div className={styles.root}>
+    <Hits hitComponent={Hit} />
+  </div>
+);
 
 export default Results;
